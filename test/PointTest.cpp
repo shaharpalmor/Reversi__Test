5aa1093cfd1792f1821c69d1b72d8aa349ca73b5
elados93 <elados1163@gmail.com>
2017-11-21 22:33:37 +0200

    Added files

---
+++ test/PointTest.cpp
@@ -0,0 +1,15 @@
+
+// Created by Elad Aharon on 21/11/17.
+// ID: 311200786
+
+#include "gtest/gtest.h"
+#include "Point.h"
+
+
+TEST(TestPoint, TestEquallty) {
+    Point p1(-1, 0);
+    Point p2(0, -1);
+    Point p3(-1, 0);
+    EXPECT_FALSE(p1.isEqual(p2));
+    EXPECT_TRUE(p1.isEqual(p3));
+}
